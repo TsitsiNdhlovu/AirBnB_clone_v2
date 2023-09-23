b865477933f2e58dcc73caca925a0851a9e5b55b
kokainaW <williamkhoko@yahoo.com>
2023-09-23 17:27:22 +0100

    init update

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
+import os
+
 from models.city import City
+from tests.test_models.test_base_model import TestBasemodel
 
 
-class test_City(test_basemodel):
-    """ """
-
+class TestCity(TestBasemodel):
+    """It represents the tests for the City model."""
     def __init__(self, *args, **kwargs):
-        """ """
+        """It initializes the test class."""
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
     def test_state_id(self):
-        """ """
+        """It tests the type of state_id."""
         new = self.value()
-        self.assertEqual(type(new.state_id), str)
+        self.assertEqual(
+            type(new.state_id),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
 
     def test_name(self):
-        """ """
+        """It tests the type of name."""
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(
+            type(new.name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
+        )
